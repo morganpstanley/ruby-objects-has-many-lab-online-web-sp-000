c4100401df94053cc2418c6992cf8a2b32645a4b
Morgan Stanley <morganpstanley@gmail.com>
2019-07-14 04:55:30 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,9 @@
 class Post
-  attr_accessor :name, :author
+  attr_accessor :title, :author
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
 
@@ -11,11 +11,11 @@ class Post
     @@all
   end
 
-  def author_name
+  def author_title
     if @author == nil
       nil
     else
-      @author.name
+      @author.title
     end
   end
 end
