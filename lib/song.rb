9c0fb7881687ddf7978f85b076f2537c05b934eb
Morgan Stanley <morganpstanley@gmail.com>
2019-07-14 04:45:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,19 @@
 class Song
   attr_accessor :name, :artist
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def artist_name
-    if @artist == nil
-      return nil
+    if @artist.name == nil
+      nil
     else
       @artist.name
     end
