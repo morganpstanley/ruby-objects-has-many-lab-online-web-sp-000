ffb7da43e4efd5fcd94a71bf7f315a8638f5145d
Morgan Stanley <morganpstanley@gmail.com>
2019-07-14 03:37:17 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+  attr_accessor :name
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
+  end
+
+  def add_song_by_name(song)
+    @songs << song
+    @@song_count += 1
+    song = Song.new(song)
+    song.artist = self
+    song.name = song
+  end
+
+  def song_count
+    @@song_count
+  end
+
+
+
+
+
+
+end
