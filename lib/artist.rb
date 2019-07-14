c1dbdb5fd7cce1d8da2164b82cd30a5e7b162150
Morgan Stanley <morganpstanley@gmail.com>
2019-07-14 03:45:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,12 +20,6 @@ class Artist
   end
 
   def add_song_by_name(song)
-    song_name = song
-    @songs << song
-    @@song_count += 1
-    song = Song.new(song)
-    song.artist = self
-    song.name = song_name
   end
 
   def song_count
